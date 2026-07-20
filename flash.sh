76f5ba386d43e86dfd519bce4e83490c5796c3bd
Sebastian Jingoi <sebi.jingoi@gmail.com>
2026-07-20 14:57:34 -0400

    removed slave

--- flash.sh
+++ flash.sh
@@ -1,9 +1,9 @@
 if [ $# -eq 0 ]; then
-    echo "Usage: flash.sh [picotool path] [slave|master]"
+    echo "Usage: flash.sh [picotool path]"
     exit 1
 fi
 
 picotool=$1
-binary=$2
+binary="master"
 
 sudo "${picotool}/build/picotool" load "./build/${binary}.elf" -fx
